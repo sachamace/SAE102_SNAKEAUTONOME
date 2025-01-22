5b0a7f98056253ce0263ac7ba47365ffe5713820
Noah-LeGoff <noahlegoff22970@gmail.com>
2025-01-22 09:07:39 +0100

    d

--- version4.c
+++ version4.c
@@ -75,12 +75,12 @@
 typedef char tPlateau[LARGEUR_PLATEAU + 1][HAUTEUR_PLATEAU + 1];
 
 // coordonnées des pommes
-const int lesPommesX[NB_POMMES] = {75, 75, 78, 2, 8, 78, 74, 2, 72, 5};
-const int lesPommesY[NB_POMMES] = { 8, 39, 2, 2, 5, 39, 33, 38, 35, 2};
+int lesPommesX[NB_POMMES] = {40, 75, 78, 2, 9, 78, 74, 2, 72, 5};
+int lesPommesY[NB_POMMES] = {20, 38, 2, 2, 5, 38, 32, 38, 32, 2};
 
 // coordonnées des pavés
-const int lesPavesX[NB_PAVES] = { 3, 74, 3, 74, 38, 38};
-const int lesPavesY[NB_PAVES] = { 3, 3, 34, 34, 21, 15};
+int lesPavesX[NB_PAVES] = { 4, 73, 4, 73, 38, 38};
+int lesPavesY[NB_PAVES] = { 4, 4, 33, 33, 14, 22};
 
 void initPlateau(tPlateau plateau);
 void dessinerPlateau(tPlateau plateau);
